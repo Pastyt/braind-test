9ade0c211378ecaa43e2a0eda99b09c3f69120fe
Pavel <pavlon.vladenkof@gmail.com>
2021-09-22 15:19:20 +0300

    Seems ok

---
+++ task_2/index.php
@@ -0,0 +1,6 @@
+
+<form action="count.php" method="get">
+  <p>N: <input type="N" name="N" /></p>
+  <p>M: <input type="M" name="M" /></p>
+  <p><input type="submit" name="submit" value="Submit" /></p>
+</form>
---
+++ task_3/index.php
@@ -0,0 +1,6 @@
+
+<form action="second.php" method="get">
+  <p>N: <input type="N" name="N" /></p>
+  <p>K: <input type="K" name="K" /></p>
+  <p><input type="submit" name="submit" value="Submit" /></p>
+</form>
